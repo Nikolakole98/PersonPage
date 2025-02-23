ebc6968f2c6a7ee9ac3030af3b5fa7de4b2b0219
Nikolakole98 <102147755+Nikolakole98@users.noreply.github.com>
2025-02-23 15:50:26 +0100

    cleanV1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import Header from "./components/Header";
-import HeroSection from "./components/MainSector/HeroSection"
-import AboutSection from "./components/MainSector/AboutSection"
-import ResumeSection from "./components/MainSector/ResumeSection";
+
+
 function App() {
   return (
-    <div className="index-page dark-background">
-      <Header/>
+    <div className="index-page">
+     {/*Header*/}
+      <header>
+      <h1>Header</h1>
+      </header>
+      {/*Main Section*/}
       <main>
-        <HeroSection/>
-        <AboutSection/>
-        <ResumeSection/>
+      {/*Hello Page*/}
+        <section>
+        <h1>HelloPage</h1>
+        </section>
+        {/*About Us*/}
+        <section>
+        <h1>About Us</h1>
+        </section>
+        {/*Skills*/}
+        <section>
+        <h1>Skills</h1>
+        </section>
+         {/*Projects*/}
+         <section>
+        <h1>Projects</h1>
+        </section>
+         {/*Contact me */}
+         <section>
+        <h1>Contact me </h1>
+        </section>
+         {/*Footer*/}
+         <footer>
+        <h1>Footer</h1>
+        </footer>        
 
       </main>
     </div>
